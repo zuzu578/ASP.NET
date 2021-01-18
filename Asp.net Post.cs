2d127e63c2c6c3b7ca15335d9e271bfa2a6ebe04
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-18 18:50:27 +0900

    asp.net Post form action

---
+++ Asp.net Post.cs
@@ -0,0 +1,26 @@
+[HttpPost]
+        public ActionResult DoLogin(UserInfo userInfo)
+        {
+            String UserId = userInfo.UserId;
+            String UserPassword = userInfo.UserPassword;
+            Console.WriteLine(UserId);
+            Console.WriteLine(UserPassword);
+
+            return View("Result", userInfo);
+        }
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+    <!-- post -->
+    <h1> @Model.UserId</h1>
+    <h1> @Model.UserPassword</h1>
+</body>
+</html>
