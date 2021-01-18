d112a4d440cf0cf1b22beaed341b5f47387fa7c8
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-18 18:44:03 +0900

    ASP FORM GET

---
+++ asp.net Get .cs
@@ -0,0 +1,30 @@
+ 
+        public ActionResult DoLogin()
+        {
+            String UserId = Request.QueryString["UserId"];
+            String UserPassword = Request.QueryString["UserPassword"];
+            ViewData["UserId"] = UserId;
+            ViewData["UserPassword"] = UserPassword;
+            Console.WriteLine(UserId);
+            Console.WriteLine(UserPassword);
+            return View("Result");
+        }
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+   
+    <h1> @ViewData["UserId"]</h1>
+    <h1> @ViewData["UserPassword"]</h1>
+</body>
+</html>
+
+
+      
