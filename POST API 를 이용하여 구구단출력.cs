8e2a5cc5cf8422b46acb587e5d1271ec5bf4588c
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-21 18:59:34 +0900

    POST API로 구구단출력

---
+++ POST API 를 이용하여 구구단출력.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using MvcCalculator.Models;
+
+namespace MvcCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Form()
+        {
+
+            return View("Form");
+        }
+        [HttpPost]
+        public ActionResult Transfer(MyNumber model)
+        {
+            int number = model.number;
+
+            return View("Result",model);
+        }
+    }
+}
+
+
+
+
+@model MvcCalculator.Models.MyNumber
+
+@{
+    Layout = null;
+}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+    <form action="Transfer" method="post">
+        <input type="text" name="number"id="number"/>
+        <input type="submit" name="submit" id="submit" value="submit" />
+    </form>
+</body>
+</html>
+
+
+
+
+
+@{
+    Layout = null;
+}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+    <h1> Calculator Result </h1>
+    @for(int i= 1 ; i<=9;i++){
+        <h1>@Model.number*@i=@(Model.number*i)</h1>
+        
+    }
+     
+</body>
+
+</html>
+
