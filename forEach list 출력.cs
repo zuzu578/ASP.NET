d6bddb98031aee3299d87dca6040cf8c280d993e
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-21 21:51:17 +0900

    ASP.NET FOREACH 로 LIST 출력(RAZOR)

---
+++ forEach list 출력.cs
@@ -0,0 +1,26 @@
+@{
+    Layout = null;
+}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List2</title>
+
+</head>
+<body>
+    @{
+        List<string> list = new List<string>();
+        list.Add("hello");
+        list.Add("world");
+
+
+    }
+    @foreach (var item in list)
+    {
+        <h1>@item</h1>
+    }
+
+</body>
+</html>
+
