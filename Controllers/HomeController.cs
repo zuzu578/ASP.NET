8abd46b9d57f10b1bc90e6db53159af80e341513
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-23 10:48:52 +0900

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,72 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
-using System.Web.Services;
-using Solution001.Models;
+using POSTAPI.Models;
 
-namespace Solution001.Controllers
+namespace POSTAPI.Controllers
 {
     public class HomeController : Controller
     {
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Index(  UserModel userModel)
+        public ActionResult Typing()
         {
-            /*
-            string Userid = Request.QueryString["Userid"];
-            string password = Request.QueryString["password"];
-            */
-            //get 방법
-            //1)get --> Request.QueryString[ ] ; --> viewData 를 이용해서 view 에 데이터 전송
-           
-            /*
-            String fname = Request.QueryString["fname"];
-            String lname = Request.QueryString["lname"];
-            */
-            //Post 방법
-            //2) post --> model 객체를 생성 , 강력한 view 페이지 생성 , return , model 참조변수를 전달 해서
-            //view 에서 @model.fname (ex) 이런식으로
-            //사용하면된다.  
-            String fname = userModel.fname;
-            String lname = userModel.lname;
-            Console.WriteLine(fname);
-            Console.WriteLine(lname);
-            if (!fname.Equals("dlwnghks") || !lname.Equals("123qwe")) 
-            {
-                Console.WriteLine("debug1");
-                Console.WriteLine(fname);
-                Console.WriteLine(lname);
-                return View("Error", userModel);
-            }
-            //get 방법 
-            /*
-            ViewData["fname"] = fname;
-          
-            ViewData["lname"] = lname;
-            */
-            //* return view( ) view 메서드안에는 리턴하고싶은 View 를 입력하면된다.
-            return View("Result", userModel);
-
-
-            //return new RedirectResult(@"~\Home\Result");
-            //return Redirect("/Home/Result");
+            return View("Typing");
         }
-        public ActionResult Error()
-        {
-            return View();
-        }
-        
         public ActionResult Result()
         {
-            return View();
+            return View("Result");
         }
-        
-        public ActionResult New()
+        [HttpPost]
+
+        public ActionResult DoPost(Userinfo model)
         {
-            
-            return View();
+            //list 출력시 
+            // List<Userinfo> a1 = new List<Userinfo>();
+            string userid = model.userid;
+            string userpassowrd = model.userpassword;
+            Console.WriteLine(userid);
+            Console.WriteLine(userpassowrd);
 
+
+            return View("Result", model);
         }
+        public ActionResult NewAjax()
+        {
+            return View("NewAjax");
+        }
+        [HttpPost]
+        public ActionResult DoAjax(Userinfo model)
+        {
 
-       
+            String userid = model.userid;
+            String userpassword = model.userpassword;
+            Console.WriteLine(userid);
+            Console.WriteLine(userpassword);
+            //json 사용법//
+            return Json(model, JsonRequestBehavior.AllowGet);
+           
+        }
     }
 }
---
+++ Models/Userinfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace POSTAPI.Models
+{
+    public class Userinfo
+    {
+        public string userid { get; set; }
+        public string userpassword { get; set; }
+    }
+}
